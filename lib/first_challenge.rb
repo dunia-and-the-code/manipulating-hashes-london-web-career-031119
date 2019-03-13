b505dbe866da628d0972bc7efb5301c4d6d20608
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-13 12:24:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,26 @@ def first_challenge
     }
   }
 
-contacts.each do |key, value|
-  if key == :favorite_icecream_flavors
-    contacts.delete_if do |value| 
-      value == "strawberry"
+contacts.collect do |key, value|
+  info.collect do |key,value|
+    
+    if key == :favorite_icecream_flavors
+       contacts.delete_if do |value| 
+          value == "strawberry"
+    end
   end
 end
+
+ 
+ 
+ contacts.collect do |person, data|
+
+     data.collect do |attribute, value|
+
+       if attribute == :favorite_icecream_flavors
+  #remember to return your newly altered contacts hash!	        value.delete("strawberry")
+      end
+    end
+  end
+  contacts	  contacts
+end	end
